09f9ab32c6fa45db6856706641630644834f0d76
Hiroto Tsuchida <tsuchida@cv.info.gifu-u.ac.jp>
2024-04-03 23:47:11 +0900

    update title

--- components/Layout.js
+++ components/Layout.js
@@ -4,8 +4,8 @@ import "@fortawesome/fontawesome-svg-core/styles.css"
 
 export default function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
-      
+    <div>
+      <title>土田 裕登</title>
       <Header />
       <main className="main">{children}</main> 
       <Footer className="footer" />
