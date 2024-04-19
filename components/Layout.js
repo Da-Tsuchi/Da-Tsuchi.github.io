f0b472e238310179bb437b43ba6da14389079084
Hiroto Tsuchida <tsuchida@cv.info.gifu-u.ac.jp>
2024-04-20 02:43:13 +0900

    update meta

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Header from './Header';
 import Footer from './Footer';
+import Head from 'next/head';
 import "@fortawesome/fontawesome-svg-core/styles.css"
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Head>
+        <meta name="google-site-verification" content="MSW7uAvAq4_hq6ioPiOafVkmWwx1707GizVeYUajhik" />
+      </Head>
       <title>土田 裕登</title>
       <Header />
       <main className="main">{children}</main> 
