b559b6f206ef547fe2baed274c5102b3d7fb719b
Hiroto Tsuchida <tsuchida@cv.info.gifu-u.ac.jp>
2025-08-27 23:28:48 +0900

    update pub

--- data/publications.js
+++ data/publications.js
@@ -2,9 +2,17 @@ export const publications = {
   domestic: [
     {
       title:
+        '視覚言語モデルによる汎用外観検査の性能向上',
+      authors: '土田 裕登，表 英輝，林 良和，上野 詩翔，加藤 邦人, et al.',
+      conf: '第30回 知能メカトロニクスワークショップ iMEC2025',
+      id: 'IM4-1',
+      date: '発表予定',
+    },
+    {
+      title:
         '大規模視覚言語モデルにおける未見タスクの効率的な追加学習法の検討',
       authors: '土田 裕登，梁瀬 和哉，加藤 邦人',
-      conf: 'サマーセミナー 2024',
+      conf: '精密工学会 画像応用技術専門委員会 サマーセミナー2024',
       id: '1-2，vol.48，pp.5-7',
       date: '2024-08-22',
       url: 'https://www.ite.or.jp/study/me/ss/2024/#program',
@@ -13,7 +21,7 @@ export const publications = {
       title:
         '大規模 Vision & Language マルチタスクモデルの学習効率化と Human-Object Interaction への適用',
       authors: '軸屋 敬介，梁瀬 和哉，表 英輝，土田 裕登，加藤 邦人',
-      conf: 'DIA 2024',
+      conf: '動的画像処理実利用化ワークショップ DIA2024',
       id: 'OS3-2',
       date: '2024-03-05',
       url: 'https://www.tc-iaip.org/dia/2024/program.html#os3',
