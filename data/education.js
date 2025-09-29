c9cf9a074bd4f04dc667b5b734140ee041f15a37
Hiroto Tsuchida <tsuchida@cv.info.gifu-u.ac.jp>
2025-09-30 02:11:14 +0900

    update edu

--- data/education.js
+++ data/education.js
@@ -1,12 +1,12 @@
 // 純粋な配列データ。URL を省略すれば “リンクなし” 表示にフォールバック。
 export const education = [
   {
-    id: 'gakushi',
-    date: '2020 – 2024',
-    title: '学士課程（工学）',
+    id: 'shushi',
+    date: '2024 – 現在',
+    title: '修士課程（工学）',
     institution: {
-      name: '岐阜大学 – 工学部 電気電子・情報工学科 情報コース',
-      url: 'https://info.gifu-u.ac.jp/',
+      name: '岐阜大学 – 自然科学技術研究科 知能理工学専攻 知能情報学領域',
+      url: 'https://gnst.gifu-u.ac.jp/intellscieng/infomatics/',
     },
   },
   {
@@ -19,12 +19,14 @@ export const education = [
     },
   },
   {
-    id: 'shushi',
-    date: '2024 – 現在',
-    title: '修士課程（工学）',
+    id: 'gakushi',
+    date: '2020 – 2024',
+    title: '学士課程（工学）',
     institution: {
-      name: '岐阜大学 – 自然科学技術研究科 知能理工学専攻 知能情報学領域',
-      url: 'https://gnst.gifu-u.ac.jp/intellscieng/infomatics/',
+      name: '岐阜大学 – 工学部 電気電子・情報工学科 情報コース',
+      url: 'https://info.gifu-u.ac.jp/',
     },
   },
+  
+  
 ];
