20d8abeeb58842898bb81d37153fd280ac8594cb
Hiroto Tsuchida <tsuchida@cv.info.gifu-u.ac.jp>
2025-09-30 02:14:04 +0900

    update edu

--- data/education.js
+++ data/education.js
@@ -2,16 +2,16 @@
 export const education = [
   {
     id: 'shushi',
-    date: '2024 – 現在',
+    date: '2024.04 – 現在',
     title: '修士課程（工学）',
     institution: {
-      name: '岐阜大学 – 自然科学技術研究科 知能理工学専攻 知能情報学領域',
+      name: '岐阜大学大学院 – 自然科学技術研究科 知能理工学専攻 知能情報学領域',
       url: 'https://gnst.gifu-u.ac.jp/intellscieng/infomatics/',
     },
   },
   {
     id: 'lab',
-    date: '2022 – 現在',
+    date: '2022.10 – 現在',
     title: '研究室配属',
     institution: {
       name: '岐阜大学 – 工学部 加藤研究室',
@@ -20,7 +20,7 @@ export const education = [
   },
   {
     id: 'gakushi',
-    date: '2020 – 2024',
+    date: '2020.04 – 2024.03',
     title: '学士課程（工学）',
     institution: {
       name: '岐阜大学 – 工学部 電気電子・情報工学科 情報コース',
